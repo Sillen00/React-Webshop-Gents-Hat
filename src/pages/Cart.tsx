d5017e7ac4792e3a44adf59969f6b05d1ba26b57
Simon <sillen.00@hotmail.com>
2023-03-11 18:52:34 +0100

    image placing right

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -1,6 +1,7 @@
 import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline'
 import KeyboardDoubleArrowDownIcon from '@mui/icons-material/KeyboardDoubleArrowDown'
 import { Box, Button, Container, Paper, Stack, Typography } from '@mui/material'
+import { CSSProperties } from 'react'
 import { products } from '../../data'
 
 function Cart() {
@@ -17,9 +18,9 @@ function Cart() {
       <Box>
         <Paper elevation={3}>
           <Stack spacing={{ xs: 1, sm: 2, md: 4 }}>
-            <Box sx={{display:"flex"}}>
-              <Box sx={{width:"10px", height:"10px"}}>
-                <img src={products[0].image} alt={products[0].description} />
+            <Box sx={{ display: 'flex' }}>
+              <Box sx={{width:"30%"}}>
+                <img style={cardImgStyle} src={products[0].image} alt={products[0].description} />
               </Box>
               <Box sx={{ display: 'flex' }}>
                 <Typography>{products[0].title}</Typography>
@@ -32,5 +33,10 @@ function Cart() {
     </Container>
   )
 }
+
+const cardImgStyle: CSSProperties = {
+  width: "100%",
+  height: "100%",
+};
 
 export default Cart
