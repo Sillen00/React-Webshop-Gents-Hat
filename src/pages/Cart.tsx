338448b2dbe0858150a0839aaad479e243165db8
Simon <sillen.00@hotmail.com>
2023-03-11 17:29:24 +0100

    Added MUI icons (npm) and start styling cart.

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -1,10 +1,17 @@
+import KeyboardDoubleArrowDownIcon from '@mui/icons-material/KeyboardDoubleArrowDown'
+import { Box, Button, Container, Typography } from '@mui/material'
+
 function Cart() {
   return (
-    <>
-      <h2>Cart</h2>
-      <p>Here is your shopping cart</p>
-    </>
-  );
+    <Container>
+      <Box sx={{ display: 'flex', justifyContent: 'space-between', py:1,  }}>
+        <Typography variant='h3'>Cart</Typography>
+        <Button variant='contained' sx={{ fontWeight: '800', color: 'common.black' }}>
+          Checkout <KeyboardDoubleArrowDownIcon />
+        </Button>
+      </Box>
+    </Container>
+  )
 }
 
-export default Cart;
+export default Cart
