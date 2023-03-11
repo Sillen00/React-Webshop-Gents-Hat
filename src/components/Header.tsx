ae887f56ef9b5c3292d72e67c60efe98c56f3775
Thomas ingvarsson <105804552+GunnarIng@users.noreply.github.com>
2023-03-11 23:23:11 +0100

    added badge and breakpoint styling.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,25 +1,37 @@
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart'
-import { AppBar, Box, SxProps, Theme, Typography } from '@mui/material'
+import { AppBar, Badge, Box, SxProps, Theme, Typography } from '@mui/material'
 import { NavLink } from 'react-router-dom'
 import adminIcon from '../../public/icons/adminIcon.png'
 
 function Header() {
   return (
     <AppBar sx={headerStyleSx}>
-      <Typography variant='h4'>Gents Hats</Typography>
+      <Typography sx={typographyStylesSX} variant='h4'>
+        Gents Hats
+      </Typography>
       <Box sx={iconWrapperStylesSX}>
         <NavLink to=''>
           <img src={adminIcon} />
         </NavLink>
         <NavLink to='/cart'>
-          {/* ToDO 
-          Add a popper for the Cart */}
-        <ShoppingCartIcon sx={iconStylesSX} />
+          <Badge sx={badgeStylesSX} badgeContent={2} color='success'>
+            <ShoppingCartIcon sx={iconStylesSX} />
+          </Badge>
         </NavLink>
       </Box>
     </AppBar>
   )
 }
+
+const typographyStylesSX: SxProps<Theme> = theme => ({
+  fontSize: '40px',
+  [theme.breakpoints.down('md')]: {
+    fontSize: '30px',
+  },
+  [theme.breakpoints.down('sm')]: {
+    fontSize: '25px',
+  },
+})
 
 const headerStyleSx: SxProps<Theme> = theme => ({
   backgroundColor: 'black',
@@ -27,23 +39,65 @@ const headerStyleSx: SxProps<Theme> = theme => ({
   display: 'flex',
   flexDirection: 'row',
   justifyContent: 'space-between',
-  alignItems: 'center',
+  alignItems: 'baseline',
   padding: '0 10px',
   position: 'static',
   borderBottom: '5px solid #DAB90C',
   // boxShadow: 'none',
+  [theme.breakpoints.down('md')]: {
+    padding: '0 5px',
+    [theme.breakpoints.down('sm')]: {
+      padding: '0 2px',
+    },
+  },
 })
 
 const iconWrapperStylesSX: SxProps<Theme> = theme => ({
   display: 'flex',
+  padding: '10px',
   alignItems: 'baseline',
   gap: '10px',
+  '& img': {
+    margin: '0 0 -15px 0',
+  },
+  [theme.breakpoints.down('md')]: {
+    '& img': {
+      width: '30px',
+      [theme.breakpoints.down('sm')]: {
+        width: '25px',
+      },
+    },
+  },
 })
 
 const iconStylesSX: SxProps<Theme> = theme => ({
   fontSize: '40px',
-  
   color: 'white',
+  position: 'relative',
+  
+  [theme.breakpoints.down('md')]: {
+    fontSize: '30px',
+    top: '3px',
+    [theme.breakpoints.down('sm')]: {
+      fontSize: '25px',
+      top: '5px',
+
+    },
+}})
+
+const badgeStylesSX: SxProps<Theme> = theme => ({
+  '& .MuiBadge-badge': {
+    color: 'white',
+   height: '22px',
+    fontSize: '18px',
+    [theme.breakpoints.down('md')]: {
+      scale: '0.8',
+      [theme.breakpoints.down('sm')]: {
+      scale: '0.6',
+      },
+    },
+  },
 })
+
 
 export default Header
