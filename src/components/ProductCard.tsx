5dfb3e2a6c16a34325e2de439701811a6d946455
Linus Hernvall <linus.hernvall@gmail.com>
2023-03-11 15:47:59 +0100

    Styled cards and media queries. Changed couple of pics.

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -15,23 +15,17 @@ export default function ProductCard({ product }: Props) {
       <CardActionArea>
         <CardMedia component='img' height='150' image={product.image} alt={product.title} />
         <CardContent>
-          <Typography variant='body2' data-cy='product-price'>
+          <Typography sx={priceTagStyle} variant='body2' data-cy='product-price'>
             $ {product.price}
           </Typography>
-          <Typography
-            color='primary'
-            gutterBottom
-            variant='h5'
-            component='div'
-            data-cy='product-title'
-          >
+          <Typography gutterBottom variant='h5' component='div' data-cy='product-title'>
             {product.title}
           </Typography>
 
           <Typography variant='body2' data-cy='product-description'>
             {product.description}
           </Typography>
-          <Button variant='contained' data-cy='product-buy-button'>
+          <Button sx={cartButtonStyle} variant='contained' data-cy='product-buy-button'>
             Add to Cart
           </Button>
         </CardContent>
@@ -42,6 +36,21 @@ export default function ProductCard({ product }: Props) {
 }
 
 const cardStyle: SxProps<Theme> = theme => ({
-  minWidth: 270,
+  minWidth: 240,
   borderRadius: 2,
+})
+
+const cartButtonStyle: SxProps<Theme> = theme => ({
+  width: '100%',
+  marginTop: '1rem',
+})
+
+const priceTagStyle: SxProps<Theme> = theme => ({
+  border: '1px solid black',
+  padding: '0.3rem 0.4rem',
+  display: 'inline',
+  background: '#D1D1D1',
+  position: 'absolute',
+  right: '1.5rem',
+  top: '8.5rem',
 })
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,11 +5,19 @@ import ProductCard from '../components/ProductCard'
 function Home() {
   return (
     <Box>
-      <Typography variant='h2'>Home</Typography>
-      <Typography>Here is home page with out products</Typography>
-      <Grid container rowSpacing={2}>
+      <Box sx={logoStyleSx}>
+        <img
+          style={{ width: '100%', maxWidth: '30rem' }}
+          src='../images/logo-gents-hat.png'
+          alt='Logotype'
+        />
+      </Box>
+      <Typography sx={h4StyleSx} variant='h4'>
+        Our Products
+      </Typography>
+      <Grid sx={cardListSx} container rowSpacing={3}>
         {products.map(product => (
-          <Grid sx={cardListSx} item xs={12} sm={6} md={4} lg={3}>
+          <Grid sx={cardListSx} item xs={12} sm={6} md={4} lg={3} xl={2}>
             <ProductCard product={product} key={product.id} />
           </Grid>
         ))}
@@ -19,9 +27,25 @@ function Home() {
 }
 
 const cardListSx: SxProps<Theme> = theme => ({
-  background: 'red',
   display: 'flex',
   justifyContent: 'center',
+})
+
+const logoStyleSx: SxProps<Theme> = theme => ({
+  display: 'flex',
+  justifyContent: 'center',
+  margin: ' 2rem 0rem',
+  '@media (min-width: 900px)': {
+    margin: ' 6rem 0rem',
+  },
+})
+
+const h4StyleSx: SxProps<Theme> = theme => ({
+  display: 'none',
+  margin: '1rem',
+  '@media (min-width: 600px)': {
+    display: 'flex',
+  },
 })
 
 export default Home
