f4e3bbaa30b889fff64fa050d018ce430d8c8851
Linus Hernvall <linus.hernvall@gmail.com>
2023-03-11 16:21:58 +0100

    Directed to product description.

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -3,6 +3,7 @@ import Card from '@mui/material/Card'
 import CardContent from '@mui/material/CardContent'
 import CardMedia from '@mui/material/CardMedia'
 import Typography from '@mui/material/Typography'
+import { Link } from 'react-router-dom'
 import { Product } from '../../data'
 
 interface Props {
@@ -11,27 +12,30 @@ interface Props {
 
 export default function ProductCard({ product }: Props) {
   return (
-    <Card sx={cardStyle} data-cy='product'>
-      <CardActionArea>
-        <CardMedia component='img' height='150' image={product.image} alt={product.title} />
-        <CardContent>
-          <Typography sx={priceTagStyle} variant='body2' data-cy='product-price'>
-            $ {product.price}
-          </Typography>
-          <Typography gutterBottom variant='h5' component='div' data-cy='product-title'>
-            {product.title}
-          </Typography>
+    <Link style={{ textDecoration: 'none' }} to={`/product/${product.id}`}>
+      <Card sx={cardStyle} data-cy='product'>
+        <CardActionArea>
+          <CardMedia component='img' height='150' image={product.image} alt={product.title} />
+          <CardContent>
+            <Typography sx={priceTagStyle} variant='body2' data-cy='product-price'>
+              $ {product.price}
+            </Typography>
+            <Typography gutterBottom variant='h5' component='div' data-cy='product-title'>
+              {product.title}
+            </Typography>
 
-          <Typography variant='body2' data-cy='product-description'>
-            {product.description}
-          </Typography>
-          <Button sx={cartButtonStyle} variant='contained' data-cy='product-buy-button'>
-            Add to Cart
-          </Button>
-        </CardContent>
-      </CardActionArea>
-    </Card>
-    // <Link style={{ textDecoration: 'none' }} to='./product-description'></Link>
+            <Typography variant='body2' data-cy='product-description'>
+              {product.description}
+            </Typography>
+            <Link style={{ textDecoration: 'none' }} to='./cart'>
+              <Button sx={cartButtonStyle} variant='contained' data-cy='product-buy-button'>
+                Add to Cart
+              </Button>
+            </Link>
+          </CardContent>
+        </CardActionArea>
+      </Card>
+    </Link>
   )
 }
 
--- src/pages/ProductDescription.tsx
+++ src/pages/ProductDescription.tsx
@@ -1,10 +1,20 @@
+import { useParams } from 'react-router-dom'
+import { Product, products } from '../../data'
+
 function ProductDescription() {
+  const { id } = useParams<{ id: string }>()
+  const product: Product | undefined = products.find(p => p.id === id)
+
+  if (!product) {
+    return <div>Product not found</div>
+  }
+
   return (
     <>
-      <h2>Product Description</h2>
-      <img src='../public/images/bowler.jpeg' />
-
-      <p>Here is the description of 1 product</p>
+      <h2>{product.title}</h2>
+      <img src={product.image} alt={product.title} />
+      <p>{product.description}</p>
+      <p>Price: $ {product.price}</p>
     </>
   )
 }
