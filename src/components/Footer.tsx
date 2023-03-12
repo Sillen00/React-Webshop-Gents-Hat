0556e64a8d7fc30afcb52a473adb764353be51c3
stenbumling <marcusrosin@outlook.com>
2023-03-12 13:19:43 +0100

    Added box shadow to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,16 +1,16 @@
 import { Box, SxProps, Theme, Typography } from '@mui/material'
-import '../index.css';
+import '../index.css'
 
 function Footer() {
   return (
     <Box sx={footerStyle}>
-      <Typography variant="h3">Gents Hats</Typography>
-      <Typography variant="h3">Gents Hats</Typography>
-      <Typography variant="h3">Gents Hats</Typography>
-      <Typography variant="h3">Gents Hats</Typography>
-      <Typography variant="h3">Gents Hats</Typography>
-      <Typography variant="h3">Gents Hats</Typography>
-      <Typography variant="h3">Gents Hats</Typography>
+      <Typography variant='h3'>Gents Hats</Typography>
+      <Typography variant='h3'>Gents Hats</Typography>
+      <Typography variant='h3'>Gents Hats</Typography>
+      <Typography variant='h3'>Gents Hats</Typography>
+      <Typography variant='h3'>Gents Hats</Typography>
+      <Typography variant='h3'>Gents Hats</Typography>
+      <Typography variant='h3'>Gents Hats</Typography>
     </Box>
   )
 }
@@ -22,7 +22,9 @@ const footerStyle: SxProps<Theme> = {
   background: 'black',
   borderTop: '5px solid #DAB90C',
   padding: '1rem',
-  overflow: 'hidden'
+  overflow: 'hidden',
+  boxShadow:
+    '0px -2px 4px -1px rgba(0,0,0,0.2), 0px -4px 5px 0px rgba(0,0,0,0.14), 0px -1px 10px 0px rgba(0,0,0,0.12)',
 }
 
 export default Footer
