46e76811c2c0c000a8fd91b8ec74fd8877387906
stenbumling <marcusrosin@outlook.com>
2023-03-12 13:12:17 +0100

    fix: Add CSS variables for easier changing of footer and header heights

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,19 +1,28 @@
 import { Box, SxProps, Theme, Typography } from '@mui/material'
+import '../index.css';
 
 function Footer() {
   return (
     <Box sx={footerStyle}>
+      <Typography variant="h3">Gents Hats</Typography>
+      <Typography variant="h3">Gents Hats</Typography>
+      <Typography variant="h3">Gents Hats</Typography>
+      <Typography variant="h3">Gents Hats</Typography>
+      <Typography variant="h3">Gents Hats</Typography>
+      <Typography variant="h3">Gents Hats</Typography>
       <Typography variant="h3">Gents Hats</Typography>
     </Box>
   )
 }
 
 const footerStyle: SxProps<Theme> = {
-  height: '18rem',
+  height: 'var(--footer-height)',
   width: '100%',
   color: 'white',
   background: 'black',
   borderTop: '5px solid #DAB90C',
+  padding: '1rem',
+  overflow: 'hidden'
 }
 
 export default Footer
