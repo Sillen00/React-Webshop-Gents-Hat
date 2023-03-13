e8be6032d65f603039d1154beb8fb43e2dbe5e2f
stenbumling <marcusrosin@outlook.com>
2023-03-13 15:14:02 +0100

    Add margin between footer and main content

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -114,6 +114,7 @@ const footerStyle: SxProps<Theme> = theme => ({
   color: 'white',
   background: 'black',
   borderTop: '5px solid #DAB90C',
+  marginTop: '4rem',
   boxShadow:
     '0px -2px 4px -1px rgba(0,0,0,0.2), 0px -4px 5px 0px rgba(0,0,0,0.14), 0px -1px 10px 0px rgba(0,0,0,0.12)',
   [theme.breakpoints.down('lg')]: {
