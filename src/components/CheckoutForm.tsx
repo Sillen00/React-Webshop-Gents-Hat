1b87765490ee78b0ea4ae198c03c47065cb709c8
stenbumling <marcusrosin@outlook.com>
2023-03-15 12:03:36 +0100

    Add yup validation to checkout form

--- src/components/CheckoutForm.tsx
+++ src/components/CheckoutForm.tsx
@@ -1,17 +1,75 @@
 import { Button, TextField } from '@mui/material'
 import { useFormik } from 'formik'
+import * as Yup from 'yup'
+
+const checkoutFormSchema = Yup.object().shape({
+  fullName: Yup.string()
+    .required('Please tell us your full name.')
+    .min(
+      5,
+      'The name you have given us it too short. Please give us a name of minimum 5 characters.'
+    ),
+  email: Yup.string()
+    .email("Your e-mail seems to be incorrectly formatted. Please make sure it's correct.")
+    .required('Please tell us your email.')
+    .min(
+      7,
+      'The e-mail you have given us it too short. Please give us an e-mail of minimum 7 characters.'
+    ),
+  phoneNumber: Yup.string()
+    .required('Please tell us your phone number.')
+    .matches(
+      /^[0-9]*$/,
+      'Your phone number must be numeric. Please give us a correctly formatted phone number.'
+    )
+    .min(
+      8,
+      'The phone number you have given us it too short. Please give us a phone number of minimum 8 characters.'
+    )
+    .max(
+      15,
+      'The phone number you have given us it too long. Please give us a phone number of maximum 15 characters.'
+    ),
+  address: Yup.string()
+    .required('Please tell us on what address you currently live at.')
+    .min(
+      6,
+      'The address you have given us it too short. Please give us an address of minimum 6 characters.'
+    )
+    .max(
+      15,
+      'The address you have given us it too long. Please give us an address of maximum 60 characters.'
+    ),
+  postalCode: Yup.string()
+    .required('Please tell us your postal code.')
+    .matches(
+      /^[0-9]*$/,
+      'Your postal code must be numeric. Please give us a correctly formatted postal code.'
+    )
+    .min(
+      5,
+      'The postal code should be 5 characters long. Please make sure you have given us a correct postal code.'
+    )
+    .max(
+      5,
+      'The postal code should be 5 characters long. Please make sure you have given us a correct postal code.'
+    ),
+  city: Yup.string().required('Please tell us in which city you reside.'),
+})
+
+type checkoutFormValues = Yup.InferType<typeof checkoutFormSchema>
 
 export default function CheckoutForm() {
-  const formik = useFormik({
+  const formik = useFormik<checkoutFormValues>({
     initialValues: {
       fullName: '',
       email: '',
       phoneNumber: '',
-      adress: '',
+      address: '',
       postalCode: '',
       city: '',
     },
-
+    validationSchema: checkoutFormSchema,
     onSubmit: values => {
       alert(JSON.stringify(values, null, 2))
     },
@@ -28,7 +86,7 @@ export default function CheckoutForm() {
           onChange={formik.handleChange}
           error={formik.touched.fullName && Boolean(formik.errors.fullName)}
           helperText={formik.touched.fullName && formik.errors.fullName}
-          margin="normal"
+          margin='normal'
         />
         <TextField
           fullWidth
@@ -39,7 +97,7 @@ export default function CheckoutForm() {
           onChange={formik.handleChange}
           error={formik.touched.email && Boolean(formik.errors.email)}
           helperText={formik.touched.email && formik.errors.email}
-          margin="normal"
+          margin='normal'
         />
         <TextField
           fullWidth
@@ -50,17 +108,17 @@ export default function CheckoutForm() {
           onChange={formik.handleChange}
           error={formik.touched.phoneNumber && Boolean(formik.errors.phoneNumber)}
           helperText={formik.touched.phoneNumber && formik.errors.phoneNumber}
-          margin="normal"
+          margin='normal'
         />
         <TextField
           fullWidth
-          id='adress'
-          label='Adress'
-          value={formik.values.adress}
+          id='address'
+          label='address'
+          value={formik.values.address}
           onChange={formik.handleChange}
-          error={formik.touched.adress && Boolean(formik.errors.adress)}
-          helperText={formik.touched.adress && formik.errors.adress}
-          margin="normal"
+          error={formik.touched.address && Boolean(formik.errors.address)}
+          helperText={formik.touched.address && formik.errors.address}
+          margin='normal'
         />
         <TextField
           fullWidth
@@ -70,7 +128,7 @@ export default function CheckoutForm() {
           onChange={formik.handleChange}
           error={formik.touched.postalCode && Boolean(formik.errors.postalCode)}
           helperText={formik.touched.postalCode && formik.errors.postalCode}
-          margin="normal"
+          margin='normal'
         />
         <TextField
           fullWidth
@@ -80,7 +138,7 @@ export default function CheckoutForm() {
           onChange={formik.handleChange}
           error={formik.touched.city && Boolean(formik.errors.city)}
           helperText={formik.touched.city && formik.errors.city}
-          margin="normal"
+          margin='normal'
         />
         <Button color='primary' variant='contained' fullWidth type='submit'>
           Place order
