6ed1c61dbaad458452a63708c2b8e9a005994e9b
stenbumling <marcusrosin@outlook.com>
2023-03-16 13:41:44 +0100

    Improved product spacings on home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { Box, Grid, SxProps, Theme, Typography } from '@mui/material'
+import { Box, Container, Grid, SxProps, Theme, Typography } from '@mui/material'
 import { products } from '../../data'
 import ProductCard from '../components/ProductCard'
 
@@ -12,16 +12,18 @@ function Home() {
           alt='Logotype'
         />
       </Box>
-      <Typography sx={h4StyleSx} variant='h4'>
+      <Container>
+      <Typography sx={h3StyleSx} variant='h3' gutterBottom>
         Our Products
       </Typography>
-      <Grid sx={cardListSx} container rowSpacing={3}>
+      <Grid sx={cardListSx} container rowSpacing={5}>
         {products.map(product => (
-          <Grid sx={cardListSx} item xs={12} sm={6} md={4} lg={3} xl={2}>
+          <Grid sx={cardListSx} item xs={12} sm={6} md={4} lg={3} xl={3}>
             <ProductCard product={product} key={product.id} />
           </Grid>
         ))}
       </Grid>
+      </Container>
     </Box>
   )
 }
@@ -34,15 +36,15 @@ const cardListSx: SxProps<Theme> = theme => ({
 const logoStyleSx: SxProps<Theme> = theme => ({
   display: 'flex',
   justifyContent: 'center',
-  margin: ' 2rem 0rem',
+  margin: '2rem 0rem',
   [theme.breakpoints.up('md')]: {
     margin: ' 6rem 0rem',
   },
 })
 
-const h4StyleSx: SxProps<Theme> = theme => ({
+const h3StyleSx: SxProps<Theme> = theme => ({
   display: 'none',
-  margin: '1rem',
+  margin: '2rem',
   [theme.breakpoints.up('sm')]: {
     display: 'flex',
   },
