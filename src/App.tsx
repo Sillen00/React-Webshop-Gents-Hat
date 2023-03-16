077175b41b195c7e31070b442af3ec9a1541babb
stenbumling <marcusrosin@outlook.com>
2023-03-16 15:44:45 +0100

    Add scroll to top-hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,25 @@
-import { Outlet } from "react-router-dom";
-import Footer from "./components/Footer";
-import Header from "./components/Header";
+import { Outlet } from 'react-router-dom'
+import Footer from './components/Footer'
+import Header from './components/Header'
+import ScrollToTop from './hooks/ScrollToTop'
 
 function App() {
   return (
-    <>
-      <Header />
+    <div>
+      <ScrollToTop />
+      <header>
+        <Header />
+      </header>
 
       <main>
         <Outlet />
       </main>
+
       <footer>
         <Footer />
       </footer>
-    </>
-  );
+    </div>
+  )
 }
 
-export default App;
+export default App
